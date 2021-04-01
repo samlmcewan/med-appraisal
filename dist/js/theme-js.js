42d62d8be8240268df09f97447cbfc07b47a00c6
samlmcewan <samlmcewan@gmail.com>
2021-04-01 12:39:37 +0100

    Initial commit

---
+++ dist/js/theme-js.js
@@ -0,0 +1,64 @@
+/**
+   * Testimonials slider
+   */
+ new Swiper('.testimonials-slider', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 40
+      },
+
+      1200: {
+        slidesPerView: 3,
+      }
+    }
+  });
+
+/**
+   * Gallery Slider
+   */
+ new Swiper('.gallery-slider', {
+  speed: 400,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false
+  },
+  slidesPerView: 'auto',
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 30
+    },
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+    992: {
+      slidesPerView: 5,
+      spaceBetween: 30
+    },
+    1200: {
+      slidesPerView: 7,
+      spaceBetween: 30
+    }
+  }
+});
